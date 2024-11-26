5e9b6df5eba197ffaf2af734a68ab6c2e6c67a75
aeSpike <spike.z.vitalis@gmail.com>
2024-11-26 21:42:57 +0000

    first commit

---
+++ code/win32_handmade.cpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+int CALLBACK
+WinMain(HINSTANCE hInstance,
+        HINSTANCE hPrevInstance,
+        LPSTR IpCmdLin,
+        int nCmdShow)
+{
+    MessageBox(0, "test test", "Test", MB_OK|MB_ICONINFORMATION);
+
+	return(0);
+}
