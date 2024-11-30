be6ba9abfac0dcd6b3a518a732225cc2ab763ffd
aeSpike <spike.z.vitalis@gmail.com>
2024-11-30 12:06:49 +0000

    day 004 - animating backbuffer pt.3: 32bit

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -37,22 +37,15 @@ RenderWeirdGradient(int XOffset, int YOffset)
     	Y < BitmapHeight;
 		++Y)
     {
-        uint8 *Pixel = (uint8 *)Row;
+        uint32 *Pixel = (uint32 *)Row;
         for(int X = 0;
             X < BitmapWidth;
             ++X)
         {
-            *Pixel = (uint8)(X + XOffset);
-            ++Pixel;
+			uint8 Blue = (X+XOffset);
+			uint8 Green = (Y+YOffset);
 
-            *Pixel = 0;
-            ++Pixel;
-
-            *Pixel = (uint8)(Y + YOffset);
-            ++Pixel;
-
-            *Pixel = 0;
-            ++Pixel;
+            *Pixel++ = ((Green << 8) | Blue);
         }
 
         Row += Pitch;
